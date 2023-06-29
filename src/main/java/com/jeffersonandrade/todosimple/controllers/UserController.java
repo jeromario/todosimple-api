dfe7ca81dd785ce8347f24f0a8812ae3e4b3a110
origin <https://github.com/jeromario/desafio_poo.git>
2023-06-29 11:04:54 -0300

    Create UserController

---
+++ src/main/java/com/jeffersonandrade/todosimple/controllers/UserController.java
@@ -0,0 +1,51 @@
+package com.jeffersonandrade.todosimple.controllers;
+
+import com.jeffersonandrade.todosimple.models.User;
+import com.jeffersonandrade.todosimple.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/user")
+@Validated
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User>findById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    @Validated(User.CreateUser.class)
+    public ResponseEntity<Void>create(@Valid @RequestBody User user){
+        userService.create(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+
+    }
+
+    @PutMapping("/{id}")
+    @Validated(User.UpdateUser.class)
+    public ResponseEntity<Void>update(@Valid @RequestBody User user,@PathVariable Long id){
+        user.setId(id);
+        userService.update(user);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void>delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
